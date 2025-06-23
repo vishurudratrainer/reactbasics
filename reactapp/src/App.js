b5ecd618d2ce7d6109810ab720f5a6be9dd97c86
unknown <vishurudra@gmail.com>
2025-06-23 12:30:20 +0530

    done

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import StudentForm from "./components/StudentForm";
 export default function App() {
   return (
     <div className="App">
      <Counter/>
+     <StudentForm/>
     </div>
   );
 }
---
+++ reactapp/src/components/StudentForm.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Table1 from "./Table1";
+
+export default function StudentForm() {
+  const [studentId, setStudentId] = useState(0);
+  const [name, setName] = useState("");
+  const [student, setStudent] = useState([]);
+  const captureId = (e) => setStudentId(e.target.value);
+  const captureName = (e) => setName(e.target.value);
+  const addStudent = (e) => {
+    e.preventDefault(); //this is done to stop the form from submitting
+    setStudent(student.concat({ id: studentId, name: name }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={addStudent}>
+        <label>
+          Enter ur id <input onChange={captureId} required type="number" />
+        </label>
+        <br />
+        <label>
+          Enter ur name <input onChange={captureName} required />
+        </label>
+        <button> Add user</button>
+        <br />
+      </form>
+      <Table1 tableData={student}/>
+    </div>
+  );
+}
