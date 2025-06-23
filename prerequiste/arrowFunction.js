44452ad384e5ab8ee48f4ab2bec03b88cac7ce6e
unknown <vishurudra@gmail.com>
2025-06-23 10:39:53 +0530

    done

---
+++ prerequiste/arrowFunction.js
@@ -0,0 +1,17 @@
+
+
+function add(a,b){
+    return a+b
+}
+
+
+const sum = (a,b) => a+b
+
+console.log(sum(22,33))
+
+
+const square =x=>x*x
+console.log(square(2))
+
+const printHello = ()=> console.log("Hello World")
+printHello()
---
+++ prerequiste/pureFunctions.js
@@ -0,0 +1,20 @@
+
+
+let room = {color:"red",length:20,width:20}
+
+
+const changeColor = (room,newColor)=>{
+  //  room.color =newColor
+  let copiedRoom = Object.assign({},room,{color:newColor,length:40})
+    return copiedRoom
+}
+const changeColor1 = (room,newColor)=>{
+  //  room.color =newColor
+  let copiedRoom = {...room,color:newColor,length:40}
+    return copiedRoom
+}
+
+console.log(changeColor(room,"yellow"))
+console.log(changeColor1(room,"yellow"))
+
+console.log(room)
