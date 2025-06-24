5b49be2940f1d05a77c30679b6b7e005debaf7a9
unknown <vishurudra@gmail.com>
2025-06-24 16:41:05 +0530

    2 day

--- reactredux1/src/components/Counter.js
+++ reactredux1/src/components/Counter.js
@@ -2,13 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function Counter() {
   const dispatch = useDispatch();
-  const counterData = useSelector(state => state.counter.count);
+  const counterData = useSelector((state) => state.counter.count);
   const inc = () => dispatch({ type: "INC" });
+  const dec = () => dispatch({ type: "DEC" });
+
   return (
     <div>
       <h1>Counter :{counterData}</h1>
       <br />
       <button onClick={inc}>INC</button>
+      <button onClick={dec}>DEC</button>
     </div>
   );
 }
--- reactredux1/src/reducers/CountReducer.js
+++ reactredux1/src/reducers/CountReducer.js
@@ -3,6 +3,8 @@ export default function CountReducer(state = { count: 0 }, action) {
     switch (action.type) {
       case "INC":
         return { ...state, count: state.count + 1 };
+       case "DEC":
+        return { ...state, count: state.count - 1 };
       default:
         return state;
     }
