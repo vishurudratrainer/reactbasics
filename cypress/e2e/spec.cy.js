747646d8fb0fd2074d7492c10e4b6ebfda0d979e
unknown <vishurudra@gmail.com>
2025-06-25 16:45:10 +0530

    3 day

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,6 +8,9 @@ describe('check whether spec', () => {
     cy.get("#root > div > div > div:nth-child(2) > form > label:nth-child(3) > input[type=number]").type("30")
     cy.get("#root > div > div > div:nth-child(2) > form > button:nth-child(5)").click()
     cy.get("#root > div > div > div:nth-child(2) > form > h1").contains("50")
+    cy.get("#root > div > div > div:nth-child(1) > a:nth-child(21)").click()
+    cy.get("#root > div > div > div:nth-child(2) > button").click()
+    cy.get("#root > div > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(3)").contains("delectus")
     
   })
 })
