8b8d2115ca5f609cca09f780b6fe4dca76f92109
unknown <vishurudra@gmail.com>
2025-06-25 15:39:40 +0530

    3 day

--- reacteg1typescript/src/Todos.tsx
+++ reacteg1typescript/src/Todos.tsx
@@ -7,6 +7,7 @@ interface TodosProps {
 }
 export default function Todos() {
   const [todos, setTodos] = useState<TodosProps[]>([]);
+  const [todoId, setTodoId] = useState<string>("");
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     fetch("https://jsonplaceholder.typicode.com/todos/")
@@ -14,12 +15,31 @@ export default function Todos() {
       .then((data) => setTodos(data));
   };
 
+    const handleClickById = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    fetch("https://jsonplaceholder.typicode.com/todos/"+todoId)
+      .then((data) => data.json())
+      .then((data) => setTodos([data]));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setTodoId(e.target.value);
+
   return (
     <div>
+      <label>
+        Enter input <input onChange={handleChange} />
+      </label>
+      <button onClick={handleClickById}>FETCH TODO BY ID</button><br/>
+
       <button onClick={handleClick}>FETCH TODO</button>
       <table border={1}>
         <thead>
-            <tr><th>ID</th><th>USERID</th><th>TITLE</th><th>COMPLETED</th></tr>
+          <tr>
+            <th>ID</th>
+            <th>USERID</th>
+            <th>TITLE</th>
+            <th>COMPLETED</th>
+          </tr>
         </thead>
         <tbody>
           {todos.map((row) => (
