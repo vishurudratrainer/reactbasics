113081a9ab9834e3d0be0ecd48c2e1775f5c4105
unknown <vishurudra@gmail.com>
2025-06-25 15:10:16 +0530

    3 day

--- reactreduxsaga/src/Todo.js
+++ reactreduxsaga/src/Todo.js
@@ -1,25 +1,28 @@
-
-
 import { useDispatch, useSelector } from "react-redux";
+import { FETCH_TODO, CAPTURE_TODO_ID, FETCH_TODO_BY_ID } from "./Actions";
 
+const Todo = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.todos);
+  const sendTodo = () => dispatch({ type: FETCH_TODO });
+  const captureId = (e) =>
+    dispatch({ type: CAPTURE_TODO_ID, todoId: e.target.value });
+  const sendTodoById = () =>
+    dispatch({ type: FETCH_TODO_BY_ID, todoId: data.todoId });
 
-const Todo =()=>{
+  return (
+    <div>
+      <button onClick={sendTodo}>FETCH_TODO</button>
+      <br />
+      <label>
+        Enter id <input onChange={captureId} />
+      </label>
+      <br />
+      <button onClick={sendTodoById}>FETCH TODO BY ID</button>
+      <br />
+      <h1>{JSON.stringify(data)}</h1>
+    </div>
+  );
+};
 
-    const dispatch = useDispatch()
-    const data = useSelector(state=>state.todos)
-    const sendTodo=()=>dispatch({type:"FETCH_TODO"})
-    const captureId =e => dispatch({type:"CAPTURE_TODO_ID",todoId:e.target.value})
-    const sendTodoById=()=>dispatch({type:"FETCH_TODO_BY_ID",todoId:data.todoId})
-
-    return(<div>
-
-        <button onClick={sendTodo}>FETCH_TODO</button><br/>
-        <label>Enter id <input onChange={captureId}/></label><br/>
-        <button onClick={sendTodoById}>FETCH TODO BY ID</button><br/>
-        <h1>{JSON.stringify(data)}</h1>
-
-    </div>)
-
-}
-
-export default Todo
+export default Todo;
