c3403822b429dc16e0be829c24acd2c3e9151da0
unknown <vishurudra@gmail.com>
2025-06-25 10:50:17 +0530

    3 day

--- reactredux1/src/reducers/PostReducer.js
+++ reactredux1/src/reducers/PostReducer.js
@@ -2,7 +2,7 @@ export default function PostReducer(state = { formData: {}, res: {} }, action) {
   if (action) {
     if (action.type === "POST_DATA_CAPTURE") {//this action type is to capture input
       let formDataFromAction = action.formData;
-      let modifiedData = Object.assign({}, state.formData, formDataFromAction);
+      let modifiedData = Object.assign({}, state.formData, formDataFromAction);//This extra copy is to add the properties from action to state
       return { ...state, formData: modifiedData };
     }
     if (action.type === "SAVED_POST_DATA") { //this is executed when data issaved
