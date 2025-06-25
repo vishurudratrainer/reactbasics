ad4bd34f7ab88fb9eba8884a5f1dc67b227e9393
unknown <vishurudra@gmail.com>
2025-06-25 14:57:14 +0530

    3 day

--- reactapp/src/components/reducer/TodosWithReducer.js
+++ reactapp/src/components/reducer/TodosWithReducer.js
@@ -1,9 +1,16 @@
 import { useReducer, useEffect } from "react";
 
-const TodosReducer = (state = { todos: [], doFetch: false }, action) => {
+const TodosReducer = (
+  state = { todos: [], doFetch: false, todoId: "" },
+  action
+) => {
   if (action) {
     if (action.type === "FETCH_TODO") {
       return { ...state, todos: [], doFetch: true };
+    }
+
+    if (action.type === "CAPTURE_TODO_ID") {
+      return { ...state, todos: [], doFetch: false, todoId: action.todoId };
     }
 
     if (action.type === "FETCHED_TODOS") {
@@ -19,15 +26,22 @@ export default function TodosWithReducer() {
 
   useEffect(() => {
     if (data?.doFetch) {
-      fetch("https://jsonplaceholder.typicode.com/todos")
+      let url = "https://jsonplaceholder.typicode.com/todos";
+      if (data.todoId !== "") url = url + "/" + data.todoId;
+      fetch(url)
         .then((data) => data.json())
         .then((data) => dispatch({ type: "FETCHED_TODOS", data: data }));
     }
-  }, [data?.doFetch]);
+  }, [data?.doFetch, data?.todoId]);
+  const capture = (e) =>
+    dispatch({ type: "CAPTURE_TODO_ID", todoId: e.target.value });
 
   return (
     <div>
-      <button onClick={fetchTodo}></button>
+      <button onClick={fetchTodo}>FETCH TODO USING REDUCER</button>
+      <label>
+        Enter TODO ID <input onChange={capture} />
+      </label>
       <h1>{JSON.stringify(data)}</h1>
     </div>
   );
